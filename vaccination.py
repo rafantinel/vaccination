4665866c61084054ab33fc33e59340fd98d1f5b7
Raoni Fantinel <raonifantinel@gmail.com>
2022-01-28 12:44:07 -0300

    update vaccination.py and out.csv

--- vaccination.py
+++ vaccination.py
@@ -18,23 +18,20 @@ header = ["date", "daily_vaccinations", "total_vaccinations"]
 # Write output file
 with open("out.csv", "w", encoding="utf-8", newline="") as output:
     for i in range(0, data.shape[0]):
+        # Current row
         row = data.iloc[i]
-        if row[1] > N:
+        # Calculate daliy vaccinations
+        daily = row[1] - data.iloc[i - 1][1]
+        if daily > N:
             # CSV writer
             writer = csv.writer(output)
-            # Previous value
-            pval = data.iloc[i - 1][1]
             # Loop index
             index += 1
             if index == 1:
                 # Write header
                 writer.writerow(header)
-
             # Split date variable   
             split_date = row[0].split("-")
             # Format date to "dd/mm/yyyy"
             format_date = f"{split_date[2]}/{split_date[1]}/{split_date[0]}"
-
-            # Calculate daily vacinations
-            daily = row[1] - pval
             writer.writerow([format_date, daily, row[1]])
